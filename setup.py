37921ffed1d170a0390895c897ad299546671e0f
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2019-10-25 11:49:33 +0100

    initial commit: - upgarde to python3 - exit gracefully added - changed to run as an imported package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="oxo",
+    version="2.0.0",
+    author="Adam Harrison",
+    author_email="author@example.com",
+    description="naughts and crosses game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tbd",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
